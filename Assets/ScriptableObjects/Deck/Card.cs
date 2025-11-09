5a9de0ae34867af377eb5e548499ecca89d72842
QuiqueGamboa <enriqueagam5@gmail.com>
2025-11-08 22:15:37 -0600

    Created Teleport System

--- Assets/ScriptableObjects/Deck/Card.cs
+++ Assets/ScriptableObjects/Deck/Card.cs
@@ -5,16 +5,21 @@ public class Card : ScriptableObject
 {
     public enum Suit
     {
-        Hearts,
-        Diamonds,
-        Clubs,
-        Spades
+        Hearts, //Heal from damage
+        Diamonds, //Chance to gain some gold when hitting enemies
+        Clubs, //Reduce Damage of enemies
+        Spades //Larger Bullets
     }
 
     public Sprite sprite;
     public Suit suit;
     public int number;
-
+    public bool slow; //Slow enemies
+    public bool bleed; //Puts a bleed on enemies
+    public bool pierce; //Shots can go through multiple enemies
+    public bool knockOut; //Enemies are knocked out for a few seconds
+    public bool energyRegain; //Reduce time of dash
+    public bool faster; //Bullets are faster
 
 }
 
---
+++ Assets/Scripts/Teleport Script.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    [SerializeField] GameObject teleportPosition;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject teleportInfo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && teleportInfo.activeSelf)
+        {
+            Teleport();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        teleportInfo.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        teleportInfo.SetActive(false);
+    }
+
+    void Teleport()
+    {
+        player.transform.position = teleportPosition.transform.position;
+    }
+}
