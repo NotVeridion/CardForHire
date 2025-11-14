9577c36b0213d9367025dfd7d61ff6e94c272ad7
Matthew Nguyen <m.nguyen032204@gmail.com>
2025-11-13 19:38:34 -0600

    Added basic dashing (Also TMPro essentials apparently)

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,20 +1,32 @@
 using UnityEngine;
+using System.Collections;
+using TMPro;
+using System.Data;
 
 public class PlayerScript : MonoBehaviour
 {
     public float playerMoveSpeed;
+    public float dashPower;
+    public float dashDuration;
+    public float dashCooldown;
+    private Rigidbody2D rb;
+    private bool canDash;
+    private bool isDashing;
     private float vertical;
     private float horizontal;
     private Vector3 movementVector;  
     private Animator playerAnimator;
     private SpriteRenderer playerSpriteRenderer;
     private SpriteRenderer gunSpriteRenderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         playerSpriteRenderer = GetComponent<SpriteRenderer>();
         gunSpriteRenderer = GameObject.FindWithTag("Gun").GetComponent<SpriteRenderer>();
+        canDash = true;
     }
 
     // Update is called once per frame
@@ -35,17 +47,35 @@ public class PlayerScript : MonoBehaviour
             playerSpriteRenderer.flipX = true;
             gunSpriteRenderer.flipY = true;
         }
+
+        if (!isDashing)
+        {
+            Move();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(nameof(Dash));
+        }
     }
 
     void FixedUpdate()
     {
-        Move();
+        // if (!isDashing)
+        // {
+        //     Move();
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        // {
+        //     StartCoroutine(nameof(Dash));
+        // }
+
     }
 
     void Move()
     {
-
-        transform.position += movementVector * playerMoveSpeed * Time.fixedDeltaTime;
+        rb.linearVelocity = new Vector3(movementVector.x * playerMoveSpeed, movementVector.y * playerMoveSpeed, 0);
 
         if (vertical != 0 || horizontal != 0)
         {
@@ -57,4 +87,18 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
+    IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+
+        rb.linearVelocity = new Vector3(movementVector.x * dashPower, movementVector.y * dashPower, 0);
+
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+
+        canDash = true;
+    }
 }
