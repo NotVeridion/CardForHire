58bcc5cc651ceaa85d3008330191f6a0644fdafb
QuiqueGamboa <enriqueagam5@gmail.com>
2025-11-20 19:34:33 -0600

    Created Spawner

--- Assets/Scripts/Teleport Script.cs
+++ Assets/Scripts/Teleport Script.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TeleportScript : MonoBehaviour
 {
@@ -6,10 +7,25 @@ public class TeleportScript : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] GameObject teleportInfo;
 
+    [SerializeField] bool dangerous;
+    [SerializeField] bool spawn;
+    [SerializeField] List<GameObject> spawnPositions;
+    [SerializeField] List<GameObject> enemiesPrefab;
+    public List<GameObject> currentEnemies;
+    public GameObject spawnHolder;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        if (spawn)
+        {
+            for (int i = 0; i < spawnHolder.transform.childCount; i++)
+            {
+                spawnPositions.Add(spawnHolder.transform.GetChild(i).gameObject);
+            }
+        }
     }
 
     private void Update()
@@ -34,5 +50,36 @@ public class TeleportScript : MonoBehaviour
     {
         player.transform.position = teleportPosition.transform.position;
         teleportInfo.SetActive(false );
+        if(dangerous)
+        {
+            if (spawn)
+            {
+                SpawnEnemies();
+            }
+            else
+            {
+                DeSpawnEnemies();
+            }
+        }
     }
+
+    void SpawnEnemies()
+    {
+        foreach(GameObject point in spawnPositions)
+        {
+            currentEnemies.Add(Instantiate(enemiesPrefab[Random.Range(0, enemiesPrefab.Count)], transform));
+        }
+    }
+
+    void DeSpawnEnemies()
+    {
+        currentEnemies = teleportPosition.GetComponent<TeleportScript>().currentEnemies;
+        foreach (GameObject enemy in currentEnemies)
+        {
+            Destroy(enemy);
+        }
+        currentEnemies.Clear();
+        teleportPosition.GetComponent<TeleportScript>().currentEnemies.Clear();
+    }
+
 }
